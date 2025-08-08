f5971e80c9e3cb69da4d469480fe45a00333726b
ramtechnow20 <bvhss20@gmail.com>
2025-08-08 14:23:16 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/image_search%20using%20unsplash%20api/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
